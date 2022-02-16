84825cb766b496caa486f0a17657c778dec0e245
seladb <pcapplusplus@gmail.com>
2022-02-15 23:17:11 -0800

    Commit 2

--- test.py
+++ test.py
@@ -1,11 +1,13 @@
 import shlex
 import sys
 
+
 def echo(phrase: str) -> None:
-   """A dummy wrapper around print."""
-   # for demonstration purposes, you can imagine that there is some
-   # valuable and reusable logic inside this function
-   print(phrase)
+    """A dummy wrapper around print."""
+    # for demonstration purposes, you can imagine that there is some
+    # valuable and reusable logic inside this function
+    print(phrase)
+
 
 def main() -> int:
     """Echo the input arguments to standard output"""
@@ -13,5 +15,6 @@ def main() -> int:
     echo(phrase)
     return 0
 
-if __name__ == '__main__':       
+
+if __name__ == "__main__":
     sys.exit(main())  # next section explains the use of sys.exit
